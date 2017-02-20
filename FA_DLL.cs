1a624f14ae05790a9de5eb137a1ce22b9b304ba0
ollikauppinen <olli_kauppinen@luukku.com>
2017-02-20 23:10:36 +0200

    Initial Commit

---
+++ FA_DLL.cs
@@ -0,0 +1,140 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AllcodeBuggyXbox
+{
+    class FA_DLL
+    {
+        [DllImport("FASlave.dll")]
+        public static extern char FA_ComOpen(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_ComClose(char Port);
+
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LEDWrite(char Port, char value);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LEDOn(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LEDOff(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_ReadSwitch(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_SetMotors(char Port, int left, int right);
+        [DllImport("FASlave.dll")]
+        public static extern uint FA_ReadIR(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern uint FA_ReadLine(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern uint FA_ReadLight(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_Forwards(char Port, uint distance);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_Backwards(char Port, uint distance);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_Left(char Port, uint angle);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_Right(char Port, uint angle);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_SetLogoSpeed(char Port, char speed);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_EncoderReset(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern uint FA_EncoderRead(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDClear(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDPrintString(char Port, char x, char y, byte[] text); 
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDPrintNumber(char Port, char x, char y, int number);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDDrawPixel(char Port, char x, char y, char state);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDDrawLine(char Port, char x1, char y1, char x2, char y2);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDDrawRect(char Port, char x1, char y1, char x2, char y2);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDBacklight(char Port, char value);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDVerbose(char Port, char value);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_LCDOptions(char Port, char FG, char BG, char Trans);
+
+        [DllImport("FASlave.dll")]
+        public static extern uint FA_ReadMic(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_PlayNote(char Port, uint note, uint time);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_GetAPIVersion(char Port);
+
+        [DllImport("FASlave.dll")]
+        public static extern void FA_ServoEnable(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_ServoDisable(char Port, char index);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_ServoSetPosition(char Port, char index, char position);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_ServoAutoMoveToPosition(char Port, char index, char position);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_ServoSetAutoMoveSpeed(char Port, char speed);
+
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardInit(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardCreate(char Port, byte[] filename);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardOpen(char Port, byte[] filename);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardDelete(char Port, byte[] filename);
+        [DllImport("FASlave.dll")]
+        public static extern void FA_CardWriteByte(char Port, char data);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardReadByte(char Port);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardRecordMic(char Port, char bitdepth, char samplerate, uint time, byte[] filename);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardPlayback(char Port, byte[] filename);
+        [DllImport("FASlave.dll")]
+        public static extern char FA_CardBitmap(char Port, char x, char y, byte[] filename);
+
+        [DllImport("FASlave.dll")]
+        public static extern int FA_ReadAxis(char Port, char index);
+
+
+
+        //String Functions with conversions
+        public static void FA_LCDPrintString(char Port, int x, int y, string text)
+        {
+            FA_LCDPrintString(Port, (char) x, (char) y, System.Text.Encoding.UTF8.GetBytes(text));
+        }
+
+        public static char FA_CardCreate(char Port, string filename)
+        {
+            return FA_CardCreate(Port, System.Text.Encoding.UTF8.GetBytes(filename));
+        }
+
+        public static char FA_CardOpen(char Port, string filename)
+        {
+            return FA_CardOpen(Port, System.Text.Encoding.UTF8.GetBytes(filename));
+        }
+
+        public static char FA_CardDelete(char Port, string filename)
+        {
+            return FA_CardDelete(Port, System.Text.Encoding.UTF8.GetBytes(filename));
+        }
+
+        public static char FA_CardRecordMic(char Port, char bitdepth, char samplerate, uint time, string filename)
+        {
+            return FA_CardRecordMic(Port, bitdepth, samplerate, time, System.Text.Encoding.UTF8.GetBytes(filename));
+        }
+
+        public static char FA_CardPlayback(char Port, string filename)
+        {
+            return FA_CardPlayback(Port, System.Text.Encoding.UTF8.GetBytes(filename));
+        }
+
+        public static char FA_CardBitmap(char Port, char x, char y, string filename)
+        {
+            return FA_CardBitmap(Port, x, y, System.Text.Encoding.UTF8.GetBytes(filename));
+        }
+
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Gaming.Input;
+using Windows.UI.Popups;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AllcodeBuggyXbox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        float x = 0, y = 0;
+        double right, left;
+        string buttonPressed;
+        char portNo, status;
+        int leftSpeed, rightSpeed;
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        public async void StartControl() {
+            
+            bool drive = true;
+
+            //FA_DLL.FA_ComOpen(portNo);
+
+            while (drive == true)
+            {
+
+                await GetPadInput();
+
+                try
+                {
+                    leftSpeed = (int)Math.Round(left * 100);
+                    rightSpeed = (int)Math.Round(right * 100);
+                }
+                catch
+                {
+
+                }
+
+
+                if (buttonPressed == "Y")
+                {
+                    drive = false;
+                    startButton.Content = $"Start";
+                }
+
+                leftTrigger_text.Text = left.ToString();
+                rightTrigger_text.Text = right.ToString();
+                leftMotor_text.Text = leftSpeed.ToString();
+                rightMotor_text.Text = rightSpeed.ToString();
+
+                await Drive();
+               
+            }
+            
+
+            
+        }
+
+
+        private async Task Drive()
+        {
+            try
+            {
+                FA_DLL.FA_ComOpen(portNo);
+                status = FA_DLL.FA_ComOpen(portNo);
+                COM_status.Text= status.ToString();
+            }
+            catch
+            {
+                status = 'e';
+                COM_status.Text = status.ToString();
+            }
+
+            if (status == '0')
+            {
+
+
+                FA_DLL.FA_SetMotors(portNo, leftSpeed, rightSpeed);
+            }
+
+            await Task.Delay(10);
+
+        }
+
+        private async Task GetPadInput()
+        {
+            GamepadReading pad = Gamepad.Gamepads.FirstOrDefault().GetCurrentReading();
+            right = pad.RightTrigger;
+            left = pad.LeftTrigger;
+
+            buttonPressed = pad.Buttons.ToString();
+
+            await Task.Delay(50);
+        }
+
+        private void COM_choose_LostFocus(object sender, RoutedEventArgs e)
+        {
+            portNo = char.Parse(COM_choose.Text);
+        }
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageDialog controllerFound = new MessageDialog("Controller connected?");
+            bool goToDrive = false;
+
+
+            if (Gamepad.Gamepads.Count > 0)
+            {
+                controllerFound.Commands.Add(new UICommand("Pad detected! Click to start!") { Id = 0 });
+                goToDrive = true;
+            }
+            else
+            {
+                controllerFound.Commands.Add(new UICommand("No pad detected.. Try again!") { Id = 1 });
+                goToDrive = false;
+            }
+
+            controllerFound.DefaultCommandIndex = 0;
+            controllerFound.CancelCommandIndex = 1;
+
+            var result = await controllerFound.ShowAsync();
+
+            var btn = sender as Button;
+
+            if (goToDrive == true)
+            {
+                btn.Content = $"Press Y to quit";
+                StartControl();
+               
+            }
+        }
+
+    }
+}
